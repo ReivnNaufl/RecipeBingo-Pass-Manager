52f64b350696f859458d9006be727ac5c1e1d663
Muhammad Reivan N M <reivnnaufl@gmail.com>
2025-05-24 14:40:53 +0700

    change port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import keyListHandler from './api/keyList.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
