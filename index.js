a5f24f83e8eb16d4dac124a348b334a744df8c4a
Muhammad Reivan N M <reivnnaufl@gmail.com>
2025-05-28 14:12:53 +0700

    vm version

--- index.js
+++ index.js
@@ -126,13 +126,13 @@ app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  res.status(200).sendFile('index.html')
-})
+// app.get('/', (req, res) => {
+//   res.status(200).sendFile('index.html')
+// })
 
-// app.get(/^\/(?!api).*/, (req, res) => {
-//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-// }); 
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+}); 
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
