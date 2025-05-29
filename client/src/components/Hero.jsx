66af207f141e7a6acc8bcfbe5ac3544b9e153bb9
yuzid <yazid.fauzan9999@gmail.com>
2025-05-29 23:11:15 +0700

    add logo and bit change of landingpage

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import icondownload from '../assets/icon_download.svg'
+import icongithub from '../assets/mdi_github.svg'
 
 function Hero() {
   return (
@@ -9,27 +10,34 @@ function Hero() {
           Solusi Untuk Makanan anda
         </h1>
         <p className="mt-6 text-lg text-gray-600">
-          
+          {/* Tambahkan deskripsi jika perlu */}
         </p>
         <div className="mt-8 flex justify-center gap-4">
+          {/* Tombol Unduh */}
           <a
-          href="https://github.com/ReivnNaufl/RecipeBingo/releases/download/v0.1.0/RecipeBingo.apk"
-          className="bg-green-600 text-white px-6 py-3 rounded-full text-lg hover:bg-green-700 transition flex items-center gap-2"
-        >
-          <img
-            src={icondownload}
-            alt="Logo"
-            className="w-6 h-6"
-          />
-          Unduh Sekarang
-        </a>
+            href="https://github.com/ReivnNaufl/RecipeBingo/releases/download/v0.1.0/RecipeBingo.apk"
+            className="bg-green-600 text-white px-6 py-3 rounded-full text-lg hover:bg-green-700 transition flex items-center gap-2"
+          >
+            <img
+              src={icondownload}
+              alt="Download Icon"
+              className="w-6 h-6"
+            />
+            Unduh Sekarang
+          </a>
 
+          {/* Tombol GitHub */}
           <a
             href="https://github.com/ReivnNaufl/RecipeBingo/"
-            className="border border-green-600 text-green-600 px-6 py-3 rounded-full text-lg hover:bg-green-100 transition"
+            className="border border-green-600 text-green-600 px-6 py-3 rounded-full text-lg hover:bg-green-100 transition flex items-center gap-2"
           >
-            Hubungi Kami
-        </a>
+            <img
+              src={icongithub}
+              alt="GitHub Icon"
+              className="w-6 h-6"
+            />
+            GitHub
+          </a>
         </div>
       </div>
     </section>
